a90e08b5140fee916f4e77e6c8c1f2c9c50ddeac
Petra Jaros <peeja@peeja.com>
2025-02-25 11:22:06 -0500

    Implement publishing after PR merge

--- release.ts
+++ release.ts
@@ -1,6 +1,7 @@
 import { releaseVersion, releaseChangelog, releasePublish } from 'nx/release';
 import { simpleGit } from 'simple-git';
 import { Octokit } from 'octokit';
+import npmFetch from 'npm-registry-fetch';
 import { createOrUpdateReleasePR } from './gh.js';
 
 const envVar = (name: string) => {
@@ -12,12 +13,6 @@ const envVar = (name: string) => {
   return value;
 };
 
-const REPO_OWNER = envVar('GITHUB_REPOSITORY_OWNER');
-const REPO_NAME = envVar('GITHUB_REPOSITORY_NAME');
-const MAIN_BRANCH = envVar('MAIN_BRANCH_NAME');
-const RELEASE_BRANCH = envVar('RELEASE_BRANCH_NAME');
-const octokit = new Octokit({ auth: envVar('GITHUB_TOKEN') });
-
 const versionResult = await releaseVersion({});
 console.log(versionResult);
 
@@ -25,20 +20,29 @@ const pendingVersions = Object.values(versionResult.projectsVersionData).some(
   (versionData) => versionData.newVersion
 );
 
+const git = simpleGit();
+
 if (pendingVersions) {
   console.log("There are pending versions. Let's create a release PR.");
-  await simpleGit().addConfig('user.email', 'rachabot@storacha.network');
-  await simpleGit().addConfig('user.name', 'Rachabot');
-  await simpleGit().checkoutLocalBranch(RELEASE_BRANCH);
+
+  const REPO_OWNER = envVar('GITHUB_REPOSITORY_OWNER');
+  const REPO_NAME = envVar('GITHUB_REPOSITORY_NAME');
+  const MAIN_BRANCH = envVar('MAIN_BRANCH_NAME');
+  const RELEASE_BRANCH = envVar('RELEASE_BRANCH_NAME');
+  const octokit = new Octokit({ auth: envVar('GITHUB_TOKEN') });
+
+  await git.addConfig('user.email', 'rachabot@storacha.network');
+  await git.addConfig('user.name', 'Rachabot');
+  await git.checkoutLocalBranch(RELEASE_BRANCH);
   const changelogResult = await releaseChangelog({
     versionData: versionResult.projectsVersionData,
     deleteVersionPlans: true,
     createRelease: false,
     gitPush: false,
   });
-  console.log(await simpleGit().status());
+  console.log(await git.status());
   console.log(changelogResult);
-  await simpleGit().push('origin', RELEASE_BRANCH, { '--force': null });
+  await git.push('origin', RELEASE_BRANCH, { '--force': null });
   const changelogs = Object.entries(
     changelogResult.projectChangelogs ?? {}
   ).map(([project, changelog]) =>
@@ -61,8 +65,74 @@ if (pendingVersions) {
   });
 } else {
   console.log("There are no pending versions. Let's publish the release.");
-  const publishResult = await releasePublish({ dryRun: true });
+
+  const newProjectsVersionData = Object.fromEntries(
+    await Promise.all(
+      Object.entries(versionResult.projectsVersionData).map(
+        async (entry): Promise<typeof entry> => {
+          const [project, versionData] = entry;
+          const latestPublishedVersion = await npmFetch
+            .json(`/${project}/latest`)
+            .then((pkg) => pkg.version as string)
+            .catch((e: unknown) => {
+              if (isNotFoundError(e)) {
+                // If no version has been published, use '' as the latest version.
+                return '';
+              } else {
+                throw e;
+              }
+            });
+          if (latestPublishedVersion === versionData.currentVersion) {
+            console.log(
+              `Version ${versionData.currentVersion} already published.`
+            );
+            return [project, versionData];
+          } else {
+            console.log(
+              `Going to publish ${project}@${versionData.currentVersion}`
+            );
+            return [
+              project,
+              {
+                ...versionData,
+                currentVersion: latestPublishedVersion,
+                newVersion: versionData.currentVersion,
+              },
+            ];
+          }
+        }
+      )
+    )
+  );
+
+  console.log('newProjectsVersionData:', newProjectsVersionData);
+
+  // Rerun the changelog to create the GitHub releases and the git tags
+  await releaseChangelog({
+    // versionData: versionResultToPublish.projectsVersionData,
+    versionData: newProjectsVersionData,
+    deleteVersionPlans: true,
+    createRelease: 'github',
+    gitPush: false,
+  });
+
+  // Push the tags
+  git.pushTags('origin');
+
+  // Publish to npm
+  const publishResult = await releasePublish({});
   console.log(publishResult);
 }
 
 process.exit(0);
+
+function isNotFoundError(e: unknown) {
+  return (
+    e &&
+    typeof e === 'object' &&
+    'name' in e &&
+    e.name === 'HttpErrorGeneral' &&
+    'statusCode' in e &&
+    e.statusCode === 404
+  );
+}
